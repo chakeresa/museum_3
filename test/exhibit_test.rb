ae7e789c685d9287b60e32267c374ee770c02803
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-03 14:10:25 -0600

    Create skeletons for Patron/PatronTest, Exhibit/ExhibitTest

---
+++ test/exhibit_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+
+class ExhibitTest < Minitest::Test
+end
---
+++ test/patron_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/patron'
+
+class PatronTest < Minitest::Test
+end
