f78cc307e53cdcc547c1bf875146957130e858de
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-03 14:15:27 -0600

    Add init of Patron

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -3,4 +3,16 @@ require 'minitest/pride'
 require './lib/patron'
 
 class PatronTest < Minitest::Test
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @bob
+  end
+
+  def test_it_inits_with_name_and_spending_money
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+  end
 end
