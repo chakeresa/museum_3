a0e11e3f37e42179843f616994dbd5f83738626e
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-03 14:25:02 -0600

    Add add_exhibit method to Museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,6 +5,9 @@ require './lib/museum'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
   end
 
   def test_it_exists
@@ -14,5 +17,13 @@ class MuseumTest < Minitest::Test
   def test_it_inits_with_name_and_empty_ary_of_exhibits
     assert_equal "Denver Museum of Nature and Science", @dmns.name
     assert_equal [], @dmns.exhibits
+  end
+
+  def test_add_exhibit_adds_to_ary_of_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
   end
 end
