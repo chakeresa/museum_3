8bdc2913cf6dce676337dddde4d75ce2078f010a
Alexandra Chakeres <alexchakeres@gmail.com>
2019-04-03 14:13:45 -0600

    Add init of Exhibit

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,4 +3,16 @@ require 'minitest/pride'
 require './lib/exhibit'
 
 class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new("Gems and Minerals", 0)
+  end
+
+  def test_it_exists
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_inits_with_name_and_cost
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
 end
